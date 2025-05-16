0de267a618b6edca5b8feb27fcf567b0a8976090
alb <alberthsieh1209@gmail.com>
2025-05-16 19:37:18 +0800

    true_brute_force is complete!

--- src/brute_force.py
+++ src/brute_force.py
@@ -3,6 +3,7 @@ from itertools import product
 import time
 from itertools import batched
 
+
 def pure_brute_force(game: NonoGame):
     search_count = 0
     for idx, trial in enumerate(product(*["ox" for _ in range(25)])):
@@ -13,10 +14,30 @@ def pure_brute_force(game: NonoGame):
             break
     return search_count
 
-def brute_force(game: NonoGame):
+
+def true_brute_force(game: NonoGame):
     """
     枚舉每一個 row constraints 的可能性
     在用 product 暴力搜尋
     你應該要用到兩次 product
     """
-    pass
+    circles = ["o", "oo", "ooo", "oooo", "ooooo"]
+    needed_rows = []
+
+    for row in game.row_constraints:
+        needed = []
+        for i in row:
+            needed.append(circles[i - 1])
+        for i in range(game.m - sum(row)):
+            needed.append("x")
+        needed_rows.append(list(product(*needed)))
+
+    search_count = 0
+
+    for idx, trial in enumerate(product(*needed_rows)):
+        guess = list(trial)
+        game.puzzle = guess
+        search_count += 1
+        if game.is_correct():
+            break
+    return search_count
